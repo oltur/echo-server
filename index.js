badaf6500958a221c225d0fd7ebb24764037d339
Alex Turevski <me@turevski.com>
2017-11-21 15:23:46 +0200

    Echo functionality

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', (req, res, next) => {
+    res.send('Please use POST')
+    next();
+  }
+)
+
+app.post('/', (req, res, next) => {
+    const input = req.body;
+    console.log(input);
+    const output = {input: input, status: 'OK'};
+    res.send(output);
+    next();
+  }
+)
 
 app.listen(port, () => console.log('Example app listening on port '+ port +'!'))
